a8cd3368f226d78078a772f1bc6fa0e188b37ac5
AnishaCloud4code <106745475+AnishaCloud4code@users.noreply.github.com>
2022-06-04 11:12:00 +0530

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnishaDemoProjectMVC.Startup))]
+namespace AnishaDemoProjectMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
